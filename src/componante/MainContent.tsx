a4a3fec864a309c9fad78394f86ac35ed5e265a2
aryanDantani <dantaniaryan47@gmail.com>
2024-11-04 01:53:09 +0530

    ui_changes

---
+++ src/componante/MainContent.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+const MainContent = () => {
+  return (
+    <div className="content-conatiner">
+      <h4>Main content</h4>
+    </div>
+  );
+};
+
+export default MainContent;
---
+++ src/componante/UsersProfileSuggetions.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+const UsersProfileSuggetions = () => {
+  return (
+    <div className="userProfileSuggetion-Container">
+      <h4>UsersProfileSuggetions</h4>
+    </div>
+  );
+};
+
+export default UsersProfileSuggetions;
